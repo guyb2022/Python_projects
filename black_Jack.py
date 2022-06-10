c10aebff28f8090fd51b69b092f12ac10fc92311
Guy B <91497242+guyb2022@users.noreply.github.com>
2022-06-10 22:20:33 +0300

    Add files via upload
    Black Jack game

---
+++ black_Jack.py
@@ -0,0 +1,130 @@
+from black_jack_logo import logo
+import random
+
+print(logo)
+
+cards_pack = [11,10,10,10,10,9,8,7,6,5,4,3,2]
+
+def check_sum(hand):
+    """Inner check for each cycle of checks"""
+    sum = 0
+    for index in range(len(hand)):
+        sum += hand[index]
+    return sum
+
+def check_total_sum(hand):
+    """
+    Check the total sum of the hand
+    Take into consideration the case 11 is in the hand
+    In this case we need to check both 11=11 and 11=1
+    """
+    sum = check_sum(hand)
+    
+    if 11 not in hand:
+        return sum
+    elif 11 in hand and sum <= 21:
+        return sum
+    elif 11 in hand and sum > 21:
+        while 11 in hand:
+            hand[hand.index(11)] = 1
+            sum = check_sum(hand)
+            if sum < 22:
+                return sum
+    return sum
+        
+def take_card():
+    """Return a new card randomly"""
+    return random.choice(cards_pack)
+
+def init_players_hands(player1,player2):
+    """Init first round before taking new card"""
+    player1.append(take_card())
+    player1.append(take_card())
+    player2.append(take_card())
+    return player1, player2
+
+def play_player_hand(player_hand, player_result):
+    """
+    Create all moves for player until he decide to stop taking new card
+    Or his hand get 21 or above
+    """
+    not_end = True
+    while not_end:
+        choice = input("Do you want another card? 'Y' or 'N': ").lower()
+        if  choice == 'y':
+            player_hand.append(take_card())
+            player_result = check_total_sum(player_hand)
+            print(f'Your hand: {player_hand} - Current score: {player_result}')
+            if player_result == 21:
+                print('You Win!!!')
+                return 21
+            if player_result > 21:
+                print('You are Busted!!!')
+                return 22
+        else:
+            not_end = False
+    return player_result
+        
+def play_diller_hand(diller_hand, player_result):
+    """Create all moves for diller until he reach optimal hand"""
+    not_end = True
+    while not_end:
+        diller_hand.append(take_card())
+        diller_result = check_total_sum(diller_hand)
+        if diller_result >= 21:
+            not_end = False
+        elif diller_result >= player_result:
+            not_end = False
+    return diller_result
+
+def print_status(player_hand, player_result, diller_hand, diller_result):
+    print(f'Your hand: {player_hand} - Current score: {player_result}')
+    print(f'Diller hand: {diller_hand} - Current score: {diller_result}')
+    
+def play_game():
+    """
+    Main loop for the game. 
+    Creating all moves for player and diller
+    Handelling the outcomes and the flow
+    """
+    player_hand = []
+    diller_hand = []
+    player_hand, diller_hand = init_players_hands(player_hand, diller_hand)
+    
+    if input("Do you want to play a game? 'Y' or 'N': ").lower() == 'y':
+        continue_game = True
+    else:
+        continue_game = False
+
+    while continue_game:
+
+        player_result = check_total_sum(player_hand)
+        diller_result = check_total_sum(diller_hand)
+        print_status(player_hand, player_result, diller_hand, diller_result)
+        player_result = play_player_hand(player_hand,player_result)
+
+        if player_result == 21:
+            print("You Win!!!")
+            continue_game = False
+        elif player_result > 21:
+            continue_game = False
+        else:
+            diller_result = play_diller_hand(diller_hand,player_result)
+            print_status(player_hand, player_result, diller_hand, diller_result)
+            
+            if diller_result > 21:
+                continue_game = False
+                print('You Win!!!')
+            elif diller_result > player_result:
+                print('You Lost!!!')
+                continue_game = False
+            elif diller_result < player_result:
+                print('You Win!!!')
+                continue_game = False
+            elif diller_result == player_result:
+                print("It's a draw!!!")
+                continue_game = False
+        
+if __name__ == "__main__":
+    play_game()
+    
