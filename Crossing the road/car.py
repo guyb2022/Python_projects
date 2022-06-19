8e28b1795b6ee8572ea67237f35062a5d4277bf1
Guy B <91497242+guyb2022@users.noreply.github.com>
2022-06-19 14:27:23 +0300

    Update car.py
    Using better model for the car list object

--- Crossing the road/car.py
+++ Crossing the road/car.py
@@ -3,30 +3,37 @@ import random
 WIDTH = 800
 HEIGHT = 600
 COLORS = ['red', 'green', 'black', 'purple', 'orange', 'brown']
+SPEED_CAR = 10
 
 
-class Car(Turtle):
+class CarPool(Turtle):
     """Init class for the car"""
     def __init__(self):
         """Init score board class"""
         super().__init__()
+        self.speed_car = SPEED_CAR
+        self.hideturtle()
+        self.cars_list = []
         self.create_car()
 
-    def cars_move(self, cars):
+    def cars_move(self):
         """Move the cars to the new position"""
-        for car in cars:
-            new_x = car.xcor() - 20
-            car.goto(new_x, car.ycor())
+        for car in self.cars_list:
+            car.backward(-self.speed_car)
 
     def create_car(self):
         """Create new car"""
-        self.shape('square')
-        self.color(random.choice(COLORS))
-        self.penup()
-        self.goto(400, random.randint(-HEIGHT/2+20, HEIGHT/2-40))
-        self.speed('fastest')
-        self.setheading(180)
+        new_car = Turtle('square')
+        new_car.color(random.choice(COLORS))
+        new_car.penup()
+        new_car.goto(400, random.randint(-HEIGHT/2+60, HEIGHT/2-60))
+        new_car.speed('fastest')
+        new_car.setheading(180)
+        self.cars_list.append(new_car)
 
     def reset_position(self, x, y):
         """Reset the car position after hitting left wall"""
         self.goto(x, y)
+
+    def speed_up(self):
+        self.speed_car += 5
