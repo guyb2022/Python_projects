2208d251bdce6ca2cfa65e257ed370234860309a
Guy B <91497242+guyb2022@users.noreply.github.com>
2022-07-03 09:08:47 +0300

    Update sheety_handler.py

--- workout_activity_api/sheety_handler.py
+++ workout_activity_api/sheety_handler.py
@@ -11,6 +11,7 @@ basic = HTTPBasicAuth(os.getenv('SHT_USER'), os.getenv('SHT_PASS'))
 
 class Sheety:
     def __init__(self):
+        """Init params"""
         self.api_key = os.getenv("SHT_API")
         self.url_web = f'https://api.sheety.co/{self.api_key}/myWorkoutsGuy/workouts'
         self.date = ""
@@ -21,6 +22,7 @@ class Sheety:
         self.params = {}
 
     def set_data(self, date, time, exercise, duration, calories):
+        """Setting the params to be writen on the sheet"""
         self.date = date
         self.time = time
         self.exercise = exercise
@@ -37,4 +39,5 @@ class Sheety:
         }
 
     def post_request(self):
+        """Posting the new params to the sheet"""
         return requests.post(url=self.url_web, json=self.params, auth=basic)
